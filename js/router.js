6ca200f8ee8b5e817e81227f3d00b6b6c1cc87ad
Renan Rocha <renan.aragorn@gmail.com>
2022-05-31 09:49:34 -0300

    slimmed the code, removing unnecessary lines

--- js/router.js
+++ js/router.js
@@ -18,26 +18,6 @@ export default class Router {
     const { pathname } = window.location
   
     const route = this.routes[pathname] || this.routes[404]
-
-    // document.querySelectorAll('.menu-item').forEach(element => {
-    //   element.classList.remove("selected")
-    //   element.addEventListener('click', () => {
-    //     switch (route) {
-    //       case "/page/home.html":
-    //         element.classList.add("selected")
-    //         break
-    //       case "/page/the-universe.html":
-    //         element.classList.add("selected")
-    //         break
-    //       case "/page/exploration.html":
-    //         element.classList.add("selected")
-    //         break
-    //       default:
-    //         break
-    //     }
-    // })
-
-    // })
   
     fetch(route)
     .then(data => data.text())
