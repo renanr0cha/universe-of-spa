77d09c6cbe8e5e8e851acc48aa9c184605e19cf6
Renan Rocha <renan.aragorn@gmail.com>
2022-06-01 13:23:03 -0300

    change bg img for each section of the routes

--- js/router.js
+++ js/router.js
@@ -18,10 +18,35 @@ export default class Router {
     const { pathname } = window.location
   
     const route = this.routes[pathname] || this.routes[404]
+
+   this.changeBg(pathname)
   
     fetch(route)
     .then(data => data.text())
     .then(html => document.querySelector("#app").innerHTML = html)
+  }
+
+  changeBg(path) {
+    switch (path) {
+      case "/":
+        document.body.style.backgroundImage = "url(./images/mountains-universe-1.png"
+        break
+      case "/the-universe":
+        document.body.style.backgroundImage = "url(./images/mountains-universe-2.png"
+        break
+      case "/exploration":
+        document.body.style.backgroundImage = "url(./images/mountains-universe-3.png"
+        break
+      default:
+        break
+    }
+    this.configBg()
+  }
+
+  configBg() {
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundPosition = "center"
   }
 }
 
